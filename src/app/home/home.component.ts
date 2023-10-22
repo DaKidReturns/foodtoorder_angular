a0de8549455ece058adb64d8b4028141c5da6150
Rohit Karunakaran <rohitkarun@gmail.com>
2023-10-22 23:15:35 +0530

    Add items in home page
    Add carousal effect in homepage
    Add button in the slides of carousal
    Restored some data deleted from the previous commit

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import { RestaurantService } from '../services/restaurant.service';
+import { Restaurant } from '../models/restaurant';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+    _arrRestaurants: Restaurant[] = []
+    restaurantCount = 3;
+    restuartnat = new Restaurant()
+
+    constructor(private restaurantService: RestaurantService, private router: Router) {
+        restaurantService.getRestaurants().subscribe((data) => {
+            this._arrRestaurants = data
+
+            this._arrRestaurants.sort(() => Math.random() - 0.5)
+            console.log(this._arrRestaurants)
+            this._arrRestaurants.splice(this.restaurantCount, Infinity)
+        })
+    }
+
+    goToRestaurantsPage() {
+        this.router.navigate(['/restaurants'])
+    }
 
 }
