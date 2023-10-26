9e94ec8bb26788c98a96719ebe96d87fa39a1d11
Rohit Karunakaran <rohitkarun@gmail.com>
2023-10-26 14:53:12 +0530

    Commented user component

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -9,25 +9,30 @@ import { CartService } from '../services/cart.service';
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.scss']
 })
-export class UserComponent implements OnInit{
-  // @Input() userName:string='initial';
+export class UserComponent {
   users:User[]=[]
+  
   constructor(private userService:UserService,private cartService:CartService,private router:Router){
     userService.getUsers().subscribe(data=>{this.users = data}) 
   }
 
-
-  ngOnInit(): void{
-    // console.log(this.userName)
+  /**
+   * Navigates to the page where the user details will be shown
+   * @param id The id of the user whose details needs to be shown
+   */
+  viewUser(id:number){
+    this.router.navigate(['userdetails/'+id])
   }
 
-  viewUser(i:number){
-    this.router.navigate(['userdetails/'+i])
-  }
-  deleteUser(i:number){
-    this.userService.deleteUserById(i).subscribe((data)=>{
-      this.cartService.deleteCartById(i).subscribe()
+  /**
+   * This function calls the deleteUserById from the user service and alsoe
+   * deletes the corresponding user's cart
+   * @param id : The id of the user who needs to be deleted.
+   */
+  deleteUser(id:number){
+    this.userService.deleteUserById(id).subscribe((data)=>{
+      this.cartService.deleteCartById(id).subscribe()
     })
-    
   }
+
 }
